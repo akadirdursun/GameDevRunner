50b2fba21138bcf253ffc62e021819d30322e6d1
akadirdursun <akadirdursn@gmail.com>
2021-11-12 11:34:52 +0300

    Third Party Packages Imported

---
+++ Game Dev Runner/Assets/_ThirdPartyTools/PathCreator/Examples/Scripts/PathFollower.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+    // Moves along a path at constant speed.
+    // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
+    public class PathFollower : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float moveSpeed = 5;
+        float distanceTravelled;        
+        private void OnEnable()
+        {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (pathCreator != null)
+            {                
+                pathCreator.pathUpdated -= OnPathChanged;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (pathCreator != null)
+            {
+                distanceTravelled += moveSpeed * Time.fixedDeltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+            }
+        }
+
+        // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+        // is as close as possible to its position on the old path
+        void OnPathChanged() {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
+    }
+}

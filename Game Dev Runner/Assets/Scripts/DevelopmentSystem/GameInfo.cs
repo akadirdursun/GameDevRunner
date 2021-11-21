008a9af9054130353f9cf02fd58416e1b8853c9a
akadirdursun <akadirdursn@gmail.com>
2021-11-21 17:27:02 +0300

    Game Info Scripts

---
+++ Game Dev Runner/Assets/Scripts/DevelopmentSystem/GameInfo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevRunner
+{
+    [System.Serializable]
+    public class GameInfo
+    {
+        [SerializeField] private string gameName;
+
+        [Header("Design")]
+        [SerializeField] private int currentDesignPoint;
+        [SerializeField] private int minDesignPoint;
+        private int designMultiplier = 1;
+        private int designBaseIncrement = 1;
+
+        [Header("Tecnology")]
+        [SerializeField] private int currentTechnologyPoint;
+        [SerializeField] private int minTechnologyPoint;
+        private int technologyMultiplier = 1;
+        private int technologyBaseIncrement = 1;
+
+        [Header("Art")]
+        [SerializeField] private int currentArtPoint;
+        [SerializeField] private int minArtPoint;
+        private int artMultiplier = 1;
+        private int artBaseIncrement = 1;
+
+        [Header("Workers")]
+        private int designerCount = 0;
+        private int developerCount = 0;
+        private int artistCount = 0;        
+
+        #region Properties
+        public int DesignerCount { get => designerCount; set => designerCount = value; }
+        public int DeveloperCount { get => developerCount; set => developerCount = value; }
+        public int ArtistCount { get => artistCount; set => artistCount = value; }
+        #endregion
+        
+        public void AddPoint(CollectableTypes _type)
+        {
+            switch (_type)
+            {
+                case CollectableTypes.designPoint:
+                    int dp = (designBaseIncrement * designMultiplier);
+                    currentDesignPoint += (dp + (dp * designerCount));
+                    break;
+
+                case CollectableTypes.technologyPoint:
+                    int tp = (technologyBaseIncrement * technologyMultiplier);
+                    currentTechnologyPoint += (tp + (tp * developerCount));
+                    break;
+
+                case CollectableTypes.artPoint:
+                    int ap = (artBaseIncrement * artMultiplier);
+                    currentArtPoint += (ap + (ap * artistCount));
+                    break;
+            }
+        }
+    }
+}
---
+++ Game Dev Runner/Assets/Scripts/DevelopmentSystem/StudioInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevRunner
+{
+    [CreateAssetMenu(fileName = "StudioInfo", menuName = "StudioData")]
+    public class StudioInfo : ScriptableObject
+    {
+        [SerializeField] private string name;
+        [Header("Games")]
+        [SerializeField] private GameInfo currentGame;
+        [Space]
+        [SerializeField] private List<GameInfo> games = new List<GameInfo>();
+
+        #region Events
+        private Action newGame;
+        #endregion
+
+        #region Properties
+        public GameInfo CurrentGame
+        {
+            get => currentGame;
+            set
+            {
+                currentGame = value;
+                newGame?.Invoke();
+            }
+        }
+        #endregion
+
+        public void Initiate(string _name = "")
+        {
+            name = _name;
+        }
+    }
+}

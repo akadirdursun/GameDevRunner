f85dfda9086a3244a67f70a02dc8701bbf81e25b
akadirdursun <akadirdursn@gmail.com>
2021-11-21 17:26:36 +0300

    Collectable Script

---
+++ Game Dev Runner/Assets/Scripts/Collectables/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace GameDevRunner
+{    
+    public class Character : MonoBehaviour
+    {
+
+        [SerializeField] private Jobs job;
+        [Header("Color")]
+        [SerializeField] private Color designerColor;
+        [SerializeField] private Color developerColor;
+        [SerializeField] private Color artistColor;
+        [Header("UI")]
+        [SerializeField] private TextMeshProUGUI nameText;
+
+        #region Properties
+        public Jobs Job { get => job; }
+        #endregion
+
+        #region MonoBehaviour Methods
+        private void Start()
+        {
+            SetTheCharacter();
+        }
+        #endregion
+
+        private void SetTheCharacter()
+        {
+            switch (job)
+            {
+                case Jobs.Designer:
+                    GetComponent<MeshRenderer>().material.color = designerColor;
+                    break;
+                case Jobs.Developer:
+                    GetComponent<MeshRenderer>().material.color = developerColor;
+                    break;
+                case Jobs.Artist:
+                    GetComponent<MeshRenderer>().material.color = artistColor;
+                    break;
+            }
+
+            nameText.text = job.ToString();
+        }
+    }
+}
---
+++ Game Dev Runner/Assets/Scripts/Collectables/CollectablePoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevRunner
+{
+    public class CollectablePoints : MonoBehaviour
+    {
+        [SerializeField] private CollectableTypes collectableType;
+
+        public CollectableTypes CollectableType { get => collectableType; }
+    }
+}

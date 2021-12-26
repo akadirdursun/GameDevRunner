5e084265cc48e92b542dcfced40422c19858e31f
akadirdursun <akadirdursn@gmail.com>
2021-12-26 16:42:05 +0300

    UI

---
+++ Game Dev Runner/Assets/Scripts/UI/PopUpText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PopUpText : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        PopUpRoutine();
+    }
+
+    private void PopUpRoutine()
+    {
+        transform.DOMoveY(transform.position.y + 2f, 1f).OnComplete(() => { Destroy(gameObject); });
+    }
+}
---
+++ Game Dev Runner/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace GameDevRunner
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager instance;
+
+        [Header("Multiplier Texts")]
+        [SerializeField] private TextMeshProUGUI designMultiplierText;
+        [SerializeField] private TextMeshProUGUI technologyMultiplierText;
+        [SerializeField] private TextMeshProUGUI artMultiplierText;
+        [Space]
+        [SerializeField] private Image fillImage;
+        [SerializeField] private TextMeshProUGUI fillText;
+
+        #region MonoBehaviour Methods
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else { Destroy(this); }
+        }
+        #endregion
+
+        public void SetValue(float _value, int _maxValue)
+        {
+            fillImage.fillAmount = (1 / _maxValue) * _value;
+            fillText.text = _value + " / " + _maxValue;
+        }
+
+        public void SetNodeMultipliers(GameInfo _info)
+        {
+            designMultiplierText.text = "x" + _info.DesignMultiplier;
+            designMultiplierText.gameObject.SetActive(true);
+
+            technologyMultiplierText.text = "x" + _info.TechnologyMultiplier;
+            technologyMultiplierText.gameObject.SetActive(true);
+
+            artMultiplierText.text = "x" + _info.ArtMultiplier;
+            artMultiplierText.gameObject.SetActive(true);
+        }
+
+        public void SetFillText(int _maxValue)
+        {
+            fillText.text = "0 / " + _maxValue;
+        }
+    }
+}
